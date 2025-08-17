cd5cfe8591b233a400c49476928a450a8dbdf6e0
Yeboah Peter <peteryeboah09@gmail.com>
2025-08-17 15:56:22 +0100

    hero designed for large screens

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const observer = new IntersectionObserver(entries  => {
   if (entries[0].isIntersecting) {
    heroCon.classList.add('isAnimate')
+   hero.classList.add('isAnimateH')
   /* heroH1.classList.add('ani-two')
    bookBtnCon.classList.add('ani-three')
    heroImgCon.classList.add('ani-four')*/
    console.log('seen')
   }else {
     heroCon.classList.remove('isAnimate')
+    hero.classList.remove('isAnimateH')
    /* heroH1.classList.remove('ani-two')
     bookBtnCon.classList.remove('ani-three')
     heroImgCon.classList.remove('ani-four')*/
@@ -15,10 +17,12 @@ const observer = new IntersectionObserver(entries  => {
 })
 
 const heroCon= document.querySelector('.hero-con');
+const hero= document.querySelector('.hero');
 // const heroH1= document.querySelector('.hero h1');
 // const bookBtnCon= document.querySelector('.bookTrip-con');
 // const heroImgCon= document.querySelector('.hero-image');
 observer.observe(heroCon )
+observer.observe(hero )
 /*observer.observe(heroH1 )
 observer.observe(bookBtnCon )
 observer.observe(heroImgCon )*/
